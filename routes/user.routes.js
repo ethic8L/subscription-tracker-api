6f00052e5f75ac0c6286a7ddad62738f22819df2
tyoma <tvtyyiovsv@gmail.com>
2025-02-14 21:34:20 +0100

    first commit

---
+++ routes/user.routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+const userRouter = Router();
+
+
+userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
+
+userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
+
+userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
+
+export default userRouter;
